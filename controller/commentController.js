3d92d51bed317332e4e79d793be3c7f89fd88f76
Ilham Yusuf <ilham.0992@students.amikom.ac.id>
2022-08-03 22:47:18 +0700

    [FIX] Comment section

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,4 +1,4 @@
-const model = require('../model/commentModel');
+const model = require("../model/commentModel");
 
 const showAllComment = async (req, res) => {
 	try {
@@ -6,47 +6,46 @@ const showAllComment = async (req, res) => {
 		if (getData.rowCount > 0) {
 			res.send(getData.rows);
 		} else {
-			res.status(404).send('Data not found');
+			res.status(404).send("Data not found");
 		}
 	} catch (error) {
-		console.log(error);
-		res.status(400).send('Program error!!!');
+		res.status(400).send("Program error!!!");
 	}
 };
 
 const insertComment = async (req, res) => {
 	try {
-		const { content } = req.body;
+		const { comment, user_id } = req.body;
 		const recipe_id = req.params.id;
-		const insertData = await model.insertDataComment({ recipe_id, content });
+		const insertData = await model.insertDataComment({ recipe_id, comment, user_id });
 		if (insertData) {
-			res.send('Data added successfully');
+			res.send("Data added successfully");
 		} else {
-			res.status('400').send('Data failed to change');
+			res.status("400").send("Data failed to change");
 		}
 	} catch (error) {
 		console.log(error);
-		res.status(400).send('Program error!!!');
+		res.status(400).send("Program error!!!");
 	}
 };
 
 const updateComment = async (req, res) => {
 	try {
-		const { comment_id, content } = req.body;
+		const { comment_id, comment } = req.body;
 		const checkData = await model.getCommentById(comment_id);
 		if (checkData.rowCount > 0) {
-			let inputContent = content || checkData.rows[0]?.content;
-			const updateData = await model.updateComment(inputContent, comment_id);
+			let inputComment = content || checkData.rows[0]?.comment;
+			const updateData = await model.updateComment(inputComment, comment_id);
 			if (updateData) {
-				res.send('Data has been change successfully');
+				res.send("Data has been change successfully");
 			} else {
-				res.status(400).send('Data failed to change');
+				res.status(400).send("Data failed to change");
 			}
 		} else {
-			res.status(404).send('Data not found');
+			res.status(404).send("Data not found");
 		}
 	} catch (error) {
-		res.status(400).send('Program error!!!');
+		res.status(400).send("Program error!!!");
 	}
 };
 
@@ -58,11 +57,29 @@ const deleteComment = async (req, res) => {
 			await model.deleteComment(comment_id);
 			res.send(`Comment deleted successfully`);
 		} else {
-			res.status(400).send('Comment failed to delete');
+			res.status(400).send("Comment failed to delete");
 		}
 	} catch (error) {
-		res.status(400).send('Program error!!!');
+		res.status(400).send("Program error!!!");
 	}
 };
 
-module.exports = { showAllComment, insertComment, updateComment, deleteComment };
+const getDataByRecipeId = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const getData = await model.getCommentByRecipeId(id);
+		if (getData.rowCount > 0) {
+			console.log(getData);
+			res.send({
+				totalData: getData.rowCount,
+				result: getData.rows,
+			});
+		} else {
+			res.status(404).send("Comment failed to delete");
+		}
+	} catch (error) {
+		res.status(400).send("Program error!!!");
+	}
+};
+
+module.exports = { showAllComment, insertComment, updateComment, deleteComment, getDataByRecipeId };
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,10 +1,11 @@
-const Router = require('express').Router();
-const controller = require('../controller/commentController');
-const { authenticationToken } = require('../middleware/authenticate');
+const Router = require("express").Router();
+const controller = require("../controller/commentController");
+const { authenticationToken } = require("../middleware/authenticate");
 
-Router.get('/all-comments', controller.showAllComment);
-Router.post('/recipe/:id/comment', authenticationToken, controller.insertComment);
-Router.patch('/recipe/comment/edit', authenticationToken, controller.updateComment);
-Router.delete('/recipe/comment/delete', authenticationToken, controller.deleteComment);
+Router.get("/all-comments", controller.showAllComment);
+Router.get("/get-comment/:id", controller.getDataByRecipeId);
+Router.post("/recipe/:id/comment", authenticationToken, controller.insertComment);
+Router.patch("/recipe/comment/edit", authenticationToken, controller.updateComment);
+Router.delete("/recipe/comment/delete", authenticationToken, controller.deleteComment);
 
 module.exports = Router;
